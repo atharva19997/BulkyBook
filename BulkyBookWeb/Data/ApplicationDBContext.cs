3341e453e95a39768014a42530886fb3cca27468
atharva19997 <atharva19997@yahoo.com>
2022-08-06 18:31:13 +0530

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    //to add DB related things.
+    public class ApplicationDBContext : DbContext   //Ctrl + . will show errors. Add Entity framework core by installing package from here or manually from nuget
+    {
+        //General Syntax needed to connect with entity framework.
+        // ctor is snippet for default constructor.
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; } //It will create a Category Table with name Categories and it will have 4 columns from category table!
+        //This is known as code 1st approach. other is db 1st approach, where db is already created.
+    }
+}
---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,33 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")      // Special method that will only look inside "ConnectionStrings." We can do another block, but if special method is present, not needed.
+    ))    //you will have to add sql server manually only.
+;// we are giving options here as we wrote to give options to base class while doing applicationDBContext function
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
